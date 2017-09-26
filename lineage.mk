77621e0fed769847e0ed5095cf3f4695b272c6ca
Varun Chitre <varun.chitre15@gmail.com>
2017-09-26 14:15:35 +0000

    sprout4: O bringup

--- lineage.mk
+++ lineage.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/google/sprout4/sprout4.mk)
 
 # Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := cm_sprout4
+PRODUCT_NAME := lineage_sprout4
 PRODUCT_DEVICE :=sprout4
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
